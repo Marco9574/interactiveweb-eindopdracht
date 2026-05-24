593ec5c4184a604cd25fc587883dc291443ec9ce
Marco9574 <r1111087@student.thomasmore.be>
2026-05-24 22:42:11 +0200

    js start

--- script.js
+++ script.js
@@ -1,41 +1,108 @@
-let gameState = {
-    start: true,
-    playing: false,
-    end: false
-}
-
 let score = {
     player1: 0,
     player2: 0
 }
+
+let startGesture1 = false;
+let startGesture2 = false;
 
 let showingObject = {
     player1: false,
     player2: false
 }
 
-// if(gameState.start) {
-//     const wrapper = document.createElement("div");
-//     wrapper.classList.add("start");
+function setState(state) {
+    gameState = state;
 
-//     const startSpeler = document.createElement("div");
-//     startSpeler.classList.add("start-speler");
-//     const speler1 = document.createElement("div");
-//     const speler2 = document.createElement("div");
-//     speler1.classList.add("border");
-//     speler2.classList.add("border");
-//     speler1.innerHTML = "✊ Speler 1";
-//     speler2.innerHTML = "🤙 Speler 2";
-//     startSpeler.append(speler1, speler2);
+    document.getElementById("startScreen").hidden = true;
+    document.getElementById("gameScreen").hidden = true;
+    document.getElementById("endScreen").hidden = true;
 
-//     const tutorial = document.createElement("p");
-//     tutorial.innerHTML = `1. Kies je gebaar (✊ / 🤙)<br>
-// 2. Maak jullie startgebaar om te starten<br>
-// 3. Zoek het object<br>
-// 4. Toon jouw gebaar<br>
-// 5. Je krijgt 3s om een correct object te tonen, anders is het aan de andere speler<br>
-// 6. Eerste tot 5 wint`;
+    if (state === "start")
+        document.getElementById("startScreen").hidden = false;
 
-//     wrapper.append(startSpeler, tutorial);
-//     document.querySelector(".body-wrapper").append(wrapper);
-// }
+    if (state === "playing")
+        document.getElementById("gameScreen").hidden = false;
+
+    if (state === "end")
+        document.getElementById("endScreen").hidden = false;
+}
+
+setState("start");
+
+
+
+
+
+
+
+// More API functions here:
+// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
+
+// the link to your model provided by Teachable Machine export panel
+const URL = "./my_model/";
+
+// let model, webcam, labelContainer, maxPredictions;
+let model, webcam, maxPredictions;
+
+// Load the image model and setup the webcam
+async function init() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+
+    // load the model and metadata
+    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+    // or files from your local hard drive
+    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+
+    // Convenience function to setup a webcam
+    const flip = true; // whether to flip the webcam
+    webcam = new tmImage.Webcam(350, 350, flip); // width, height, flip
+    await webcam.setup(); // request access to the webcam
+    await webcam.play();
+    window.requestAnimationFrame(loop);
+
+    // append elements to the DOM
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
+    // labelContainer = document.getElementById("label-container");
+    for (let i = 0; i < maxPredictions; i++) { // and class labels
+        // labelContainer.appendChild(document.createElement("div"));
+    }
+}
+
+async function loop() {
+    webcam.update(); // update the webcam frame
+    await predict();
+    window.requestAnimationFrame(loop);
+
+
+
+
+
+
+    if(startGesture1 && startGesture2)
+        setState("playing");
+
+
+
+}
+
+// run the webcam image through the image model
+async function predict() {
+    // predict can take in an image, video or canvas html element
+    const prediction = await model.predict(webcam.canvas);
+    for (let i = 0; i < maxPredictions; i++) {
+        const classPrediction =
+            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+        // labelContainer.childNodes[i].innerHTML = classPrediction;
+    }
+
+    if(prediction[0].probability > 0.9)
+        startGesture1 = true;
+    if(prediction[1].probability > 0.9)
+        startGesture2 = true;
+
+    
+}
